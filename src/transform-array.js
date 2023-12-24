3609a3191b6dae0e93dc889372b6fded942b2e77
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2023-12-24 17:07:43 +0400

    fix: fix array transfrom solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -18,68 +18,72 @@ function transform(arr) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   }
 
-  const doubleNext = arr.includes('--double-next') ? '--double-next' : false;
-  let indexOfDoubleNext = arr.indexOf(doubleNext);
-  const doublePrev = arr.includes('--double-prev') ? '--double-prev' : false;
-  let indexOfDoublePrev = arr.indexOf(doublePrev);
+  const arrCopy = [...arr];
 
-  const discardNext = arr.includes('--discard-next') ? '--discard-next' : false;
-  let indexOfDiscardNext = arr.indexOf(discardNext);
-  const discardPrev = arr.includes('--discard-prev') ? '--discard-prev' : false;
-  let indexOfDiscardPrev = arr.indexOf(discardPrev);
+  const doubleNext = arrCopy.includes('--double-next') ? '--double-next' : false;
+  let indexOfDoubleNext = arrCopy.indexOf(doubleNext);
+  const doublePrev = arrCopy.includes('--double-prev') ? '--double-prev' : false;
+  let indexOfDoublePrev = arrCopy.indexOf(doublePrev);
+
+  const discardNext = arrCopy.includes('--discard-next') ? '--discard-next' : false;
+  let indexOfDiscardNext = arrCopy.indexOf(discardNext);
+  const discardPrev = arrCopy.includes('--discard-prev') ? '--discard-prev' : false;
+  let indexOfDiscardPrev = arrCopy.indexOf(discardPrev);
 
   // guard closes for interrupting cases
   if (indexOfDiscardNext + 2 === indexOfDoublePrev) {
-    arr.splice(indexOfDoublePrev, 1);
+    arrCopy.splice(indexOfDoublePrev, 1);
     indexOfDoublePrev = -1;
   }
 
   if (indexOfDiscardNext + 2 === indexOfDiscardPrev) {
-    arr.splice(indexOfDiscardPrev, 1);
+    arrCopy.splice(indexOfDiscardPrev, 1);
     indexOfDiscardPrev = -1;
   }
 
   // doubleNext
 
   if (doubleNext && indexOfDoubleNext !== 0) {
-    arr[indexOfDoubleNext] = arr[indexOfDoubleNext + 1];
+    arrCopy[indexOfDoubleNext] = arrCopy[indexOfDoubleNext + 1];
   }
 
-  if (indexOfDoubleNext === arr.length - 1) {
-    arr.splice(indexOfDoubleNext, 1);
+  if (indexOfDoubleNext === arrCopy.length - 1) {
+    arrCopy.splice(indexOfDoubleNext, 1);
   }
 
   // doublePrev
 
-  if (doublePrev && indexOfDoublePrev !== arr.length - 1) {
-    arr[indexOfDoublePrev] = arr[indexOfDoublePrev - 1];
+  if (doublePrev && indexOfDoublePrev !== arrCopy.length - 1) {
+    arrCopy[indexOfDoublePrev] = arrCopy[indexOfDoublePrev - 1];
   }
 
   if (indexOfDoublePrev === 0) {
-    arr.splice(indexOfDoublePrev, 1);
+    arrCopy.splice(indexOfDoublePrev, 1);
   }
 
   // discardNext
 
   if (discardNext && indexOfDiscardNext !== 0) {
-    arr.splice(indexOfDiscardNext, 2);
+    arrCopy.splice(indexOfDiscardNext, 2);
   }
 
-  if (indexOfDiscardNext === arr.length - 1) {
-    arr.splice(indexOfDiscardNext, 1);
+  if (indexOfDiscardNext === arrCopy.length - 1) {
+    arrCopy.splice(indexOfDiscardNext, 1);
   }
 
   // discardPrev
 
   if (discardPrev && indexOfDiscardPrev > 0) {
-    arr.splice(indexOfDiscardPrev - 1, 2);
+    arrCopy.splice(indexOfDiscardPrev - 1, 2);
   }
 
   if (indexOfDiscardPrev === 0) {
-    arr.splice(indexOfDiscardPrev, 1);
+    arrCopy.splice(indexOfDiscardPrev, 1);
   }
 
-  return arr;
+  console.log(arrCopy);
+
+  return arrCopy;
 }
 
 module.exports = {
